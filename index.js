4a8234ddad050c44584673ec06b99b9b12e8c0cd
Rodrigo Rech <rodrigohrech@gmail.com>
2018-11-01 16:02:07 -0300

    Changing PDF parser to Vision API

--- index.js
+++ index.js
@@ -6,81 +6,103 @@
  */
 // Imports the Google Cloud client library
 const PubSub = require('@google-cloud/pubsub');
+const vision = require('@google-cloud/vision').v1;
+const pdfText = require('pdf-text');
 
 const gcs = require('@google-cloud/storage')();
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
-const pdfText = require('pdf-text');
 
+// Imports the Google Cloud client libraries
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
 
 // Creates a client
 const pubsub = new PubSub();
 
 
 function publish(data, bucket) {
-  const topicName = process.env.topicName;
-  const dataBuffer = Buffer.from(data);
+    const topicName = process.env.topicName;
+    const dataBuffer = Buffer.from(data);
 
-  const customAttributes = {
-    origin: 'pdf-to-text',
-    bucket: bucket
-  };
+    const customAttributes = {
+        origin: 'pdf-to-text',
+        bucket: bucket
+    };
 
-  pubsub
-    .topic(topicName)
-    .publisher()
-    .publish(dataBuffer, customAttributes)
-    .then(results => {
-      const messageId = results[0];
-      console.log(`Message ${messageId} published.`);
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-    });
+    pubsub
+        .topic(topicName)
+        .publisher()
+        .publish(dataBuffer, customAttributes)
+        .then(results => {
+            const messageId = results[0];
+            console.log(`Message ${messageId} published.`);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
 }
 
 exports.parsePDF = (data, context) => {
-  console.log(data);
-  const file = data;
-  const fileBucket = file.bucket;
-  const filePath = file.name;
-  const contentType = file.contentType;
+    console.log(data);
+    const file = data;
+    const fileBucket = file.bucket;
+    const filePath = file.name;
+    const contentType = file.contentType;
 
-  console.log(`Processing file: ${file.name}`);
-  console.log(contentType);
-  if (!contentType.endsWith('pdf')) {
-    console.log('This is not a pdf.');
-    return null;
-  }
+    console.log(`Processing file: ${file.name}`);
+    console.log(contentType);
+    if (!contentType.endsWith('pdf')) {
+        console.log('This is not a pdf.');
+        return null;
+    }
 
-  const fileName = path.basename(filePath);
-  console.log(`fileName: ${fileName}`);
-  const bucket = gcs.bucket(fileBucket);
-  console.log(`bucket: ${bucket}`);
-  const tempFilePath = path.join(os.tmpdir(), fileName);
-  console.log(`tempFilePath: ${tempFilePath}`);
-  console.log("Downloading file.");
+    const fileName = path.basename(filePath);
+    console.log(`fileName: ${fileName}`);
+    const bucket = gcs.bucket(fileBucket);
+    console.log(`bucket: ${bucket}`);
 
-  return bucket.file(filePath).download({
-    destination: tempFilePath,
-  }).then(() => {
-    console.log("Reading File");
-    
-    var buffer = fs.readFileSync(tempFilePath)
-    
-    console.log('Image downloaded locally to', tempFilePath);
-    
-    return pdfText(buffer, function (err, chunks) {
-      console.log("PDF convertion done.")
-      if (err) {
-        console.error('ERROR:', err);
-      } else {
-        console.log(chunks);
-        publish(chunks.join(' '), fileBucket);
-      }
-    });
-  }).catch((error) => { 
-    console.log(error) 
-  });
+    console.log("Downloading file.");
+
+    const gcsSourceUri = `gs://${bucket}/${fileName}`;
+    const gcsDestinationUri = `gs://${bucket}/${fileName}.json`;
+
+    const inputConfig = {
+        // Supported mime_types are: 'application/pdf' and 'image/tiff'
+        mimeType: 'application/pdf',
+        gcsSource: {
+            uri: gcsSourceUri,
+        },
+    };
+    const outputConfig = {
+        gcsDestination: {
+            uri: gcsDestinationUri,
+        },
+    };
+    const features = [{ type: 'DOCUMENT_TEXT_DETECTION' }];
+    const request = {
+        requests: [{
+            inputConfig: inputConfig,
+            features: features,
+            outputConfig: outputConfig,
+        }, ],
+    };
+
+    return client
+        .asyncBatchAnnotateFiles(request)
+        .then(results => {
+            const operation = results[0];
+            // Get a Promise representation of the final result of the job
+            return operation.promise();
+        })
+        .then(filesResponse => {
+            const destinationUri =
+                filesResponse[0].responses[0].outputConfig.gcsDestination.uri;
+            console.log('Json saved to: ' + destinationUri);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
 };
